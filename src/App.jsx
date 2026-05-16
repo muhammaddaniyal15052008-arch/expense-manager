b4836297f3340257542fb1a3c5e1bfcd983b61cb
unknown <muhammaddaniyal15052008@gmail.com>
2026-05-16 20:38:47 +0500

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     console.log(addedExpense);
     
     return (
-        <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
+        <div className="min-h-screen bg-linear-to-br from-gray-50 to-gray-100">
             <Navbar />
             <Dashboard />
             <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-6 sm:py-8">
