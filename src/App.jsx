d97c09ac92d30700612f9378b38a725b13399c67
unknown <muhammaddaniyal15052008@gmail.com>
2026-05-16 20:32:14 +0500

    dashboard added

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
     return (
         <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100">
             <Navbar />
+            <Dashboard />
             <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-6 sm:py-8">
                 <Routes>
                     <Route path='/' element={<Dashboard />} /> 
