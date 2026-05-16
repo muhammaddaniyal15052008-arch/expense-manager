a2be0ff953179d8d42ca27d1e2ca977a7835df5d
unknown <muhammaddaniyal15052008@gmail.com>
2026-05-16 20:22:37 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/expense-manager/', 
   plugins: [react(),tailwindcss()]
-  base: '/expense-manager/',
 })
